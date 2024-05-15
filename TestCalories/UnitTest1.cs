21b7b3fc6850a32fc416cde976c01201761c011d
Zoe-Hey <150931346+Zoe-Hey@users.noreply.github.com>
2024-05-15 14:05:55 +0200

    Unit Test final

--- TestCalories/UnitTest1.cs
+++ TestCalories/UnitTest1.cs
@@ -5,19 +5,26 @@ namespace TestCalories
     [TestClass]
     public class UnitTest1
     {
+        
         [TestMethod]
         public void TestMethodTotalCalories()
         {
+            //assign values of ingredient's calories
             Recipes calorie_test = new Recipes();
-            calorie_test.CalorieWarning = new()
+            calorie_test.Ingredients = new List<Ingredients>
             {
-                { "Pasta", 200},
-                { "Cheese", 402},
+                new Ingredients 
+                { name = "Pasta", quantity = 100, calories = 131},
+                new Ingredients 
+                { name = "Cheese", quantity = 100, calories = 402},
             };
 
-            double expectedTotal = 200 + 402;
-            double actualTotal = calorie_test.Calculations();
-            Assert.AreEqual(expectedTotoal, actualTotal);
+            //act
+            double expectedTotal = 131 + 402;       //value to be expected
+            double actualTotal = calorie_test.Calculations();       //actual return value of the method
+
+            //assert
+            Assert.AreEqual(expectedTotal, actualTotal);    //method to compare doubles (test passes when equal)
 
             double calorieWarning = 300;
             if(actualTotal > calorieWarning)
